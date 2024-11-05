5a97d78414824108484a0f00f9e2bb5787cb4b7f
ubuntu <ubuntu@localhost.localdomain>
2024-11-05 15:14:32 +0800

    fix report lab2 and sys_get_time for ch6

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -124,17 +124,30 @@ pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
     // );
     // -1
     trace!("kernel: sys_get_time");
+    use crate::task::current_user_token;
+    use crate::mm::translated_byte_buffer;
     use crate::timer::get_time_us;
+
     let us = get_time_us();
+    let time_val = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
 
-    use crate::mm::vaddr_to_paddr;
-    let paddr = vaddr_to_paddr(_ts as usize);
-    let p = paddr as *mut usize;
-    unsafe { *p = us / 1_000_000; }
+    let mut src = &time_val as *const TimeVal as *const u8;
+    let dest = _ts as *const u8;
 
-    let paddr = vaddr_to_paddr((_ts as usize) + 8);
-    let p = paddr as *mut usize;
-    unsafe { *p = us % 1_000_000; }
+    let buffers = translated_byte_buffer(
+        current_user_token(), dest, core::mem::size_of::<TimeVal>());
+
+    for buffer in buffers {
+        unsafe {
+            buffer.copy_from_slice(
+                core::slice::from_raw_parts(src, buffer.len()));
+            src = src.add(buffer.len());
+        }
+    }
+
     0
 }
 
